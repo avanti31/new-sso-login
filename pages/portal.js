8fee37861bb1526752954b5deb6d84e5af8fd26a
Avanti Walchale <c192565@local.indiainfoline.com>
2021-10-07 15:00:51 +0530

    portal page

--- pages/portal.js
+++ pages/portal.js
@@ -1,27 +1,60 @@
 import { data } from "../data/data";
 import { NavSearchBar } from "../components/NavSearch";
+import { useContext, useEffect } from "react";
+// import withAuth from "../hoc/withAuth";
+import { PortalContext } from "../context/portal";
+import fetcher from "../api/fetcher";
 
 const Portal = () => {
+  const [portal, setPortalData] = useContext(PortalContext);
+
+  useEffect(() => {
+    const fetchAppDetails = async () => {
+      const { EmpId } = JSON.parse(localStorage.getItem("user"));
+      const body = {
+        UserName: EmpId,
+        PosId: "",
+        BusinessUnitName: "IIFLSL Insurance Broking",
+        BusinessUnitCode: "IIFLSLIB",
+      };
+      const result = await fetcher("APP_DETAILS_URL", { body });
+      const list = JSON.parse(result.body.responseData);
+      setPortalData(list);
+      localStorage.setItem("list", JSON.stringify(list));
+    };
+
+    fetchAppDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div className=" container   w-full mt-8">
-      <div className="grid lg:grid-cols-5  sm:grid-cols-2  md:grid-cols-3  gap-4">
-        {data.map((item) => {
+    <div className=" container mx-auto w-full mt-8">
+      <NavSearchBar/>
+      <div className=" w-full py-8 w-50 flex grid lg:grid-cols-5  sm:grid-cols-2  md:grid-cols-3  gap-4">
+        {portal && portal.map((item) => {
           return (
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-              <img className="m-auto w-3/4 h-30" src={item.icon} alt="avatar" />
+          <a
+            href={`${item.redirectUrl}`}
+            target='_blank'
+            rel='noreferrer'
+            key={`app-${item.appId}`}
+            >
+            <div className="mx-auto px-4 w-50 h-50 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800" >
+              <img className="m-auto object-contain" src={item.icon} alt="avatar" />
 
               <div className="py-5 text-center">
                 <a href="#" className="block text-gray-800 dark:text-white">
                   <span className="text-md text-gray-700 dark:text-gray-200">
-                    {item.title}
+                    {item.appName}
                   </span>
                 </a>
               </div>
             </div>
+            </a>
           );
         })}
       </div>
     </div>
+    
   );
 };
 
