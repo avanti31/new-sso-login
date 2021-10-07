3bf76a5a63d78426b06975137241814d8ecff241
abhishek-mcsam <abhishek@webinfomart.com>
2021-10-07 22:42:05 +0530

    changes into nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,20 +3,18 @@ import fetcher from "../api/fetcher";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
-
-
 export const Nav = () => {
   const router = useRouter();
   const [userName, setUserName] = useState();
   const [userEmpid, setUserEmpid] = useState();
-  const [show,setShow]=useState(false); 
- 
+  const [show, setShow] = useState(false);
+
   const logoutHandler = async () => {
     const data = {
       loginId: JSON.parse(localStorage.getItem("user")).EmpId,
       sessionId: JSON.parse(localStorage.getItem("sessionId")),
     };
-   
+
     const response = await fetcher("LOGOUT_URL", { body: data });
     if (response.body.status === 0) {
       localStorage.removeItem("user");
@@ -26,11 +24,10 @@ export const Nav = () => {
       alert("fail to logout");
     }
   };
-  
-  const dropDownHandler = () =>{
-   setShow(!show)
 
-    }
+  const dropDownHandler = () => {
+    setShow(!show);
+  };
 
   useEffect(() => {
     const user = localStorage.getItem("user");
@@ -59,45 +56,50 @@ export const Nav = () => {
 
       {/* hidden md:hidden lg:hidden  */}
       <div className="m-auto mr-8  hidden    lg:block">
-        <button className="relative z-10 lg:block p-2 text-white-700 bg-transparent text-white border:none border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:bg-gray-800 focus:outline-none" onClick={()=>dropDownHandler()}>
+        <button
+          className="relative z-10 lg:block p-2 text-white-700 bg-transparent text-white border:none border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:bg-gray-800 focus:outline-none"
+          onClick={() => dropDownHandler()}
+        >
           <h3 className="mx-2 text-sm font-medium text-white-700 dark:text-gray-200  profile">
             Khatab
           </h3>
         </button>
-        
-     {show &&    <div className="absolute right-8 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800 dropdown">
-          <a
-            href="#"
-            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            My Policies
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            My Profile
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            Reports
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            Supports
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-            onClick={() => logoutHandler()} >
-            Sign Out
-          </a>
-        </div>
-      }
+
+        {show && (
+          <div className="absolute right-8 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800 dropdown">
+            <a
+              href="#"
+              className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              My Policies
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              My Profile
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Reports
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Supports
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={() => logoutHandler()}
+            >
+              Sign Out
+            </a>
+          </div>
+        )}
       </div>
     </nav>
   );
