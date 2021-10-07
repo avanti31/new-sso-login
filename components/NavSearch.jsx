dd2d274def691010062520f38e3d71616e7dbd49
abhishek-mcsam <abhishek@webinfomart.com>
2021-10-07 15:24:58 +0530

    add style

--- components/NavSearch.jsx
+++ components/NavSearch.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { PortalContext } from "../context/portal";
-import {SearchIcon} from "@heroicons/react/solid";
-
+import { SearchIcon } from "@heroicons/react/solid";
 
 export const NavSearchBar = () => {
   // @ts-ignore
@@ -30,13 +29,16 @@ export const NavSearchBar = () => {
   }, [searchTerm]);
 
   return (
-    <div className="w-full flex justify-center py-10 px-14 bg-transparent items-center " >
-      <div className=" w-3/5 h-10 px-2 py-2 flex m-auto border-2 border-gray-200">
-      <SearchIcon className="text-base ml-0 text-gray-200"/>
-      <input className=" w-full m-auto ml-2 text-base text-gray-700 placeholder-gray-500 bg-white border-none rounded-md dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none" type='tel' placeholder='Search' onChange={handleChange} />
-      
-      
-    </div>
+    <div className="w-full flex justify-center sm:py-2 lg:py-10 px-14 bg-transparent items-center ">
+      <div className="sm:w-full lg:w-3/5 h-10 px-2 py-2 flex m-auto border-2 border-gray-200">
+        <SearchIcon className="text-base ml-0 text-gray-200" />
+        <input
+          className=" w-full m-auto ml-2 text-base text-gray-700 placeholder-gray-500 bg-white border-none rounded-md dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none"
+          type="tel"
+          placeholder="Search"
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 };
--- pages/portal.js
+++ pages/portal.js
@@ -10,17 +10,17 @@ const Portal = () => {
 
   useEffect(() => {
     const fetchAppDetails = async () => {
-      const { EmpId } = JSON.parse(localStorage.getItem("user"));
+      const Id = JSON.parse(localStorage.getItem("user"));
       const body = {
-        UserName: EmpId,
+        UserName: Id?.EmpId,
         PosId: "",
         BusinessUnitName: "IIFLSL Insurance Broking",
         BusinessUnitCode: "IIFLSLIB",
       };
       const result = await fetcher("APP_DETAILS_URL", { body });
-      const list = JSON.parse(result.body.responseData);
-      setPortalData(list);
-      localStorage.setItem("list", JSON.stringify(list));
+      const list = JSON.parse(result?.body?.responseData);
+      // setPortalData(list);
+      // localStorage.setItem("list", JSON.stringify(list));
     };
 
     fetchAppDetails();
@@ -28,33 +28,37 @@ const Portal = () => {
   }, []);
   return (
     <div className=" container mx-auto w-full mt-8">
-      <NavSearchBar/>
+      <NavSearchBar />
       <div className=" w-full py-8 w-50 flex grid lg:grid-cols-5  sm:grid-cols-2  md:grid-cols-3  gap-4">
-        {portal && portal.map((item) => {
-          return (
-          <a
-            href={`${item.redirectUrl}`}
-            target='_blank'
-            rel='noreferrer'
-            key={`app-${item.appId}`}
-            >
-            <div className="mx-auto px-4 w-50 h-50 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800" >
-              <img className="m-auto object-contain" src={item.icon} alt="avatar" />
+        {data &&
+          data.map((item) => {
+            return (
+              <a
+                href={`${item.redirectUrl}`}
+                target="_blank"
+                rel="noreferrer"
+                key={`app-${item.appId}`}
+              >
+                <div className="mx-auto px-4 w-4/5 h-full overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                  <img
+                    className="m-auto object-contain"
+                    src={item.icon}
+                    alt="avatar"
+                  />
 
-              <div className="py-5 text-center">
-                <a href="#" className="block text-gray-800 dark:text-white">
-                  <span className="text-md text-gray-700 dark:text-gray-200">
-                    {item.appName}
-                  </span>
-                </a>
-              </div>
-            </div>
-            </a>
-          );
-        })}
+                  <div className="py-5 text-center">
+                    <a href="#" className="block text-gray-800 dark:text-white">
+                      <span className="text-md text-gray-700 dark:text-gray-200">
+                        {item.appName}
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
       </div>
     </div>
-    
   );
 };
 
